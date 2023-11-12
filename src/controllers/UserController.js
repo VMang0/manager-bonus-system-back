d4bac05264ca7d661773ace5731e53bfe3284c77
VMang0 <valeriakorolkova2703@gmail.com>
2023-11-12 21:02:38 +0300

    add: users logic

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,8 +1,94 @@
-import UserService from "../services/UserService.js";
+import userService from "../services/UserService.js";
+import {validationResult} from "express-validator";
+import ApiErrors from "../exceptions/api-errors.js";
+import tokenSchema from "../entites/TokenSchema.js";
 
 class UserController {
 
-  async create(req, res){
+  async registration(req, res, next) {
+    try {
+      const errors = validationResult(req);
+       if(!errors.isEmpty()) {
+         return next(ApiErrors.BadRequest('Ошибка при валидации', errors.array()))
+       }
+      const {email, password, company, role} = req.body;
+      const userData = await userService.registration(email, password, company, role);
+      res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
+      return res.json(userData);
+    } catch (e) {
+      next(e);
+    }
+  }
+  async login(req, res,next) {
+    try {
+      const {email, password} = req.body;
+      const userData = await userService.login(email, password);
+      res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
+      return res.json(userData);
+    } catch (e) {
+      next(e);
+    }
+  }
+  async logout(req, res,next) {
+    try {
+      const {refreshToken} = req.cookies;
+      const token = userService.logout(refreshToken);
+      res.clearCookie('refreshToken');
+      return res.json(token);
+    } catch (e) {
+      next(e);
+    }
+  }
+  async activate(req, res,next) {
+    try {
+      const activationLink = req.params.link;
+      await userService.activate(activationLink);
+      return res.redirect(process.env.CLIENT_URL)
+    } catch (e) {
+      next(e);
+    }
+  }
+  async refresh(req, res,next) {
+    try {
+      const {refreshToken} = req.cookies;
+      const userData  = await userService.refresh(refreshToken);
+      res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
+      return res.json(userData);
+    } catch (e) {
+      next(e);
+    }
+  }
+  async getUsers(req, res, next) {
+    try {
+      const users = await userService.getAll();
+      return res.json(users);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async getUsersForActivate(req, res, next) {
+    try {
+      const company = req.params.company;
+      const users = await userService.getUsersForActivate(company);
+      return res.json(users);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async verifyUser(req, res, next) {
+    try {
+      const {id, ...info} = req.body;
+      const user = await userService.verify(id, info);
+      return res.json(user);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+
+  /*async create(req, res){
     await UserService.create(req.body)
       .then(user => res.json(user))
       .catch(e => res.status(500).json(e))
@@ -30,7 +116,7 @@ class UserController {
     await UserService.delete(req.params.id)
       .then(user => res.json(user))
       .catch(e => res.status(500).json(e))
-  }
+  }*/
 
 }
 
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,11 +1,110 @@
-import User from "../entites/User.js";
+import UserSchema from "../entites/UserSchema.js";
+import bcrypt from 'bcrypt';
+import {v4} from 'uuid';
+import mailService from "./MailService.js";
+import tokenService from "./TokenService.js";
+import UserDTO from "../dtos/UserDTO.js";
+import ApiErrors from "../exceptions/api-errors.js";
+import tokenSchema from "../entites/TokenSchema.js";
+import CompanySchema from "../entites/CompanySchema.js";
+import userInfoService from "./UserInfoService.js";
 
 class UserService {
 
+  async registration(email, password, company, role) {
+    const candidate = await UserSchema.findOne({email})
+    if(candidate) {
+      throw ApiErrors.BadRequest(`Пользователь с почтовым индексов ${email} уже существует`)
+    }
+    const hashPassword = await bcrypt.hash(password, 3);
+    const activationLink = v4();
+    const user = await UserSchema.create({email, password: hashPassword, activationLink, company, role});
+    //await mailService.sendActivationMail(email, `${process.env.API_URL}/api/activate/${activationLink}`);
+    const userDto = new UserDTO(user);
+    const tokens = tokenService.generateToken({...userDto});
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+    return{...tokens, user: userDto}
+  }
+
+  async activate(activationLink) {
+    const user = await UserSchema.findOne({activationLink});
+    if(!user) {
+      throw ApiErrors.BadRequest('Некорректная ссылка активация')
+    }
+    user.isActivated = true;
+    await user.save();
+  }
+
+  async login(email, password) {
+    const user = await UserSchema.findOne({ email });
+    if (!user) {
+      throw ApiErrors.BadRequest('Пользователь с таким email не найден')
+    }
+    if(!user.isActivated){
+      throw ApiErrors.BadRequest('Аккаунт не активирован менеджером')
+    }
+    const isPasswordEquals = await bcrypt.compare(password, user.password);
+    if (!isPasswordEquals) {
+      throw ApiErrors.BadRequest('Введен неверный пароль')
+    }
+    const userDto = new UserDTO(user);
+    const tokens = tokenService.generateToken({...userDto});
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+    return{...tokens, user: userDto}
+  }
+
+  async logout(refreshToken) {
+    const token = await tokenService.removeToken(refreshToken);
+    return token;
+  }
+
+  async refresh(refreshToken) {
+    if (!refreshToken) {
+      throw ApiErrors.UnauthorizedError();
+    }
+    const userData = tokenService.validationRefreshToken(refreshToken);
+    const tokenFromDb = await tokenService.findToken(refreshToken);
+    if (!userData || !tokenFromDb) {
+      throw ApiErrors.UnauthorizedError();
+    }
+    const user = await UserSchema.findById(userData.id);
+    const userDto = new UserDTO(user);
+    const tokens = tokenService.generateToken({...userDto});
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+    return {...tokens, user: userDto}
+  }
+
+  async getAll() {
+    const users = await UserSchema.find();
+    return users;
+  }
+
+
+  async getUsersForActivate(company_id) {
+    const users = await UserSchema.find({company: company_id, isActivated: false, role: 'employee'});
+    return users;
+  }
+
+  async verify(id, info) {
+    const user = await UserSchema.findById(id);
+    if(!user) {
+      throw ApiErrors.BadRequest('Пользователь не найден!')
+    }
+    if(user.isActivated) {
+      throw ApiErrors.BadRequest('Пользователь активирован!')
+    }
+    const userInfo = await userInfoService.add(info);
+    const updatedUser = await UserSchema.findByIdAndUpdate(user._id, {isActivated: true, info: userInfo._id});
+    await mailService.sendVerifyInfo(user.email);
+    const users = await UserSchema.find({company: updatedUser.company, isActivated: false, role: 'employee'});
+    return users;
+  }
+
+/*
   async create(user){
-    /*if (picture) {
+    /!*if (picture) {
       fileName = FileService.saveFile(picture);
-    }*/
+    }*!/
     const  createdUser = await User.create({...user, status: false})
     return createdUser;
   }
@@ -39,7 +138,7 @@ class UserService {
     const user = await User.findByIdAndDelete(id);
     return user;
   }
-
+*/
 }
 
 export default new UserService();
