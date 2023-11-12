f55d34fbe3ec7f4a48c3ce4969abe223ce2ba347
VMang0 <valeriakorolkova2703@gmail.com>
2023-11-12 20:59:58 +0300

    edit: add routers and middlewares

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 import mongoose from "mongoose";
-import userRouter from "./src/routers/userRouter.js";
 import fileUpload from 'express-fileupload'
-import companyRouter from "./src/routers/companyRouter.js";
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import errorMiddlewares from "./src/middlewares/error-middlewares.js";
+import userRouter from "./src/routers/user-router/index.js";
+import companyRouter from "./src/routers/company-router/index.js";
 
-const port = 5000;
-const DB_URL = 'mongodb://127.0.0.1:27017/bonus-system';
-
+const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
-app.use(express.static('static'));
+app.use(cookieParser());
+//app.use(express.static('static'));
 app.use(fileUpload({}));
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: process.env.CLIENT_URL,
+}));
 
 app.use('/api', userRouter)
-app.use('/api', companyRouter)
+app.use('/api/company', companyRouter)
+
+app.use(errorMiddlewares);
 
 async function startApp() {
   try {
-    await mongoose.connect(DB_URL);
-    app.listen(port, () => console.log('server started on port' + port))
+    await mongoose.connect(process.env.DB_URL);
+    app.listen(port, () => console.log('server started on port: ' + port))
   } catch (e) {
     console.log(e);
   }
