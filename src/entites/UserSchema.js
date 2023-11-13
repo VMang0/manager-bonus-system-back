454ec5c5712002cc2cca2039175da0b0884a4a24
VMang0 <valeriakorolkova2703@gmail.com>
2023-11-14 00:24:33 +0300

    add: date of user reg

--- src/entites/UserSchema.js
+++ src/entites/UserSchema.js
@@ -2,12 +2,13 @@ import { model, Schema } from 'mongoose';
 
 const UserSchema = new Schema({
   email: { type: String, require: true, unique: true },
-  password: { type: String, require: true },
+  password: { type: String },
   isActivated: { type: Boolean, default: false },
   activationLink: { type: String },
   role: { type: String, enum: ['admin', 'manager', 'employee'], default: 'employee' },
   company: { type: Schema.Types.ObjectId, ref: 'Company' },
   info: { type: Schema.Types.ObjectId, ref: 'UserInfo' },
+  date: { type: Date, default: new Date() },
 });
 
 export default model('User', UserSchema)
