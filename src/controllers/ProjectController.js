ff1b64d9bcaf1597fc88328e7f673aed11c8df91
VMang0 <valeriakorolkova2703@gmail.com>
2023-11-20 02:28:38 +0300

    add: project work methods

--- src/controllers/ProjectController.js
+++ src/controllers/ProjectController.js
@@ -18,5 +18,41 @@ class ProjectController {
       next(e);
     }
   }
+  async getAllUsersProject(req, res, next) {
+    try {
+      const id = req.params.id;
+      const project = await projectService.getAllUsersProject(id);
+      return res.json(project);
+    } catch (e) {
+      next(e);
+    }
+  }
+  async getProjectTeam(req, res, next) {
+    try {
+      const team = await projectService.getProjectTeam();
+      return res.json(team);
+    } catch (e) {
+      next(e);
+    }
+  }
+  async update(req, res, next) {
+    try {
+      const {teamIds, ...project} = req.body;
+      const newProject = await projectService.update(teamIds, project);
+      return res.json(newProject);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async delete(req, res, next) {
+    try {
+      const idProject = req.params.id;
+      await projectService.delete(idProject);
+      return res.json('Удаление прошло успешно');
+    } catch (e) {
+      next(e);
+    }
+  }
 }
 export default new ProjectController();
